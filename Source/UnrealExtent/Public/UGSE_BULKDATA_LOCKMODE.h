4f28717a4313c937ca46e7400b9b779e7ae40239
narknon <73571427+narknon@users.noreply.github.com>
2024-09-04 13:15:38 -0400

    Update enum

--- Source/UnrealExtent/Public/UGSE_BULKDATA_LOCKMODE.h
+++ Source/UnrealExtent/Public/UGSE_BULKDATA_LOCKMODE.h
@@ -2,9 +2,10 @@
 #include "CoreMinimal.h"
 #include "UGSE_BULKDATA_LOCKMODE.generated.h"
 
-UENUM(BlueprintType)
+UENUM(Meta = (Bitflags))
 enum class UGSE_BULKDATA_LOCKMODE : uint8 {
-    LOCK_READ_ONLY = 0,
-    LOCK_READ_WRITE,
+    LOCK_READ_ONLY = 1 << 0,
+    LOCK_READ_WRITE = 1 << 1
 };
+ENUM_CLASS_FLAGS(UGSE_BULKDATA_LOCKMODE);
 
